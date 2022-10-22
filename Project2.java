aa44bac38e0b40bfed564b56b850d1ad75954219
Jaxaar <73668604+Jaxaar@users.noreply.github.com>
2022-10-22 15:51:15 -0400

    Update Project2.java
    Ok take 2, still not finished but organized better

--- Project2.java
+++ Project2.java
@@ -1,23 +1,25 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Project2 {
 
-    ArrayList<String> pwFile;
+    private ArrayList<String> users;
+    private ArrayList<String> pwHashes;
 
 
+    public static void main(String[] args){
+        try{
+            Project2 pwc = new Project2();
+        } catch (Exception e){
+            System.out.println("File Error");
+        }
+    }
 
-    public static void main(String[] args) throws FileNotFoundException {
-
+    public Project2() throws FileNotFoundException{
         Scanner scn = new Scanner(System.in);
+        Scanner fileScn = null;
 
         boolean fileLoaded = false;
         while (!fileLoaded) {
             try {
-                Scanner fileScn = new Scanner(new File("Project2PW.txt"));
+                fileScn = new Scanner(new File("Project2PW.txt"));
                 fileLoaded = true;
             } catch (Exception e) {
                 System.out.println("Creating File");
@@ -25,10 +27,15 @@ public class Project2 {
                     PrintWriter newOut = new PrintWriter("Project2PW.txt");
                     newOut.flush();
                     newOut.close();
-                } catch(Exception epw){
+                } catch (Exception epw) {
                     throw new FileNotFoundException();
                 }
             }
+        }
+
+        while(fileScn.hasNextLine()){
+            fileScn.nextLine();
+            
         }
 
         PrintWriter out;
@@ -43,13 +50,9 @@ public class Project2 {
             String input = scn.nextLine();
             if(input.toLowerCase().equals("quit")){
                 out.flush();
-
-
+                out.close();
                 break;
             }
-
-
-
         }
     }
 
